5299f3667ba245d0129224a9e8d46d7555ed54b6
Caitlin Rowley <69302450+minim-sheba@users.noreply.github.com>
2025-08-29 19:49:30 +0100

    This version works!
    Monophonic, just dealing with note on/off and velocity. The external is recognised by Pd, the object can be created and linked, and when pressing keys on the Seaboard it looks like appropriate data is being transmitted.

--- mpe-simple/mpe-simple.cpp
+++ mpe-simple/mpe-simple.cpp
@@ -4,8 +4,8 @@ extern "C" {
     // External setup function declaration
     void mpe_in_simple_setup(void);
     
-    // Main library setup function
-    void mpe_simple_setup(void) {
+    // Main library setup function - must match library name exactly
+    void mpesimple_setup(void) {
         post("MPE Simple External v1.0");
         post("Objects: [mpe-in-simple]");
         
